20485e29745067210ebef536941567614fbe3e35
NazarenkoAnna2021.com <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-01-01 19:49:37 +0200

    error

--- controllers/validation/courses.validator.js
+++
@@ -1,6 +0,0 @@
-const Joi = require('joi');
-
-exports.coursesValidator = Joi.object().keys({
-    name: Joi.string().alphanum().required(),
-    teachers_id: Joi.number().integer().min(0).required()
-});
--- controllers/validation/marks.validator.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi');
-
-exports.marksValidator = Joi.object().keys({
-    mark: Joi.number().integer().min(0).required(),
-    student_id: Joi.number().integer().min(0).required(),
-    course_id: Joi.number().integer().min(0).required(),
-    university_id: Joi.number().integer().min(0).required(),
-});
